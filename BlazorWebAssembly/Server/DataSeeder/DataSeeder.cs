ee144df6f64ba8430819394ffae11c84c71bed3a
Waseem Ahmad Mughal <WasimAhmad@users.noreply.github.com>
2023-09-18 23:27:59 +0500

    issue fixed.

---
+++ BlazorWebAssembly/Server/DataSeeder/DataSeeder.cs
@@ -0,0 +1,44 @@
+﻿using BlazorWebAssembly.Server.Models;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorWebAssembly.Server.DataSeeder
+{
+    public class DataSeeder
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DataSeeder(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task InitializeData()
+        {
+            // Create role
+            string roleName = "Demo";
+            if (!await _roleManager.RoleExistsAsync(roleName))
+            {
+                await _roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+
+            // Create user
+            ApplicationUser user = new ApplicationUser
+            {
+                UserName = "user1@gmail.com",
+                Email = "user1@gmail.com",
+                EmailConfirmed = true
+            };
+            string password = "Password!123";
+            var userResult = await _userManager.CreateAsync(user, password);
+
+            // Assign role to user
+            if (userResult.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, roleName);
+            }
+        }
+    }
+}
--- BlazorWebAssembly/Server/Program.cs
+++ BlazorWebAssembly/Server/Program.cs
@@ -1,9 +1,13 @@
 using BlazorWebAssembly.Server.Data;
+using BlazorWebAssembly.Server.DataSeeder;
 using BlazorWebAssembly.Server.Models;
 
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
+
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,21 +15,40 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddIdentityServer()
-    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>(opt =>
+    {
+        opt.IdentityResources["openid"].UserClaims.Add("role");
+        opt.ApiResources.Single().UserClaims.Add("role");
+    });
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("role");
+
 
 builder.Services.AddAuthentication()
     .AddIdentityServerJwt();
+
+builder.Services.AddTransient<DataSeeder>();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+// Database migration
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
+    dbContext!.Database.Migrate();
+
+    var seeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
+    seeder.InitializeData().Wait();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
